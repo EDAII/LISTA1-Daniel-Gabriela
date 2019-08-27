6c0e3083b47b25ae280048a4245e6c05b72b6ce5
DanMke <danmke@hotmail.com>
2019-08-26 23:28:00 -0300

    Added graph with real data
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- trabalho1.py
+++ trabalho1.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 import random
+import time
+from functools import reduce
 # INFO: 
 ## Todos os valores ordenados
 ## Sem repetição
 
-# TODO Colocar instalação do matplotlib no readme
+MAX_REGISTRY = 10000
+
+# TODO Colocar instalação do matplotlib no readme e requirements.txt
+
+def averageVector(values):
+    return reduce(lambda a, b: a + b, values) / len(values)
 
 # GERAR NÚMEROS ALEATÓRIOS EXCLUSIVOS DENTRO DE UM INTERVALO
 def generateRandomUniqueNumbers(start, end, quantity):
@@ -14,60 +21,80 @@ def generateRandomUniqueNumbers(start, end, quantity):
 
 # BUSCA SEQUENCIAL COM SENTINELA [O(n)] (Está inserindo a sentinela na lista passada por parametro)
 def sequentialSearchSentinel(key, values):
+    init = time.time()
     i = 0
     values.append(key)
     pos = len(values) - 1
     while (values[i] != key):
         i += 1
     if i == (len(values) -1):
-        return False
-    return True
+        return -1
+    return time.time() - init
 
 # BUSCA SEQUENCIAL SEM SENTINELA [O(n)]
 def sequentialSearch(key, values):
+    init = time.time()
     for i in values:
         if (key == i):
-            return True
-    return False
+            return time.time() - init
+    return -1
 
 # BUSCA BINÁRIA (COM VETOR) [O(log n)]
 def binarySearch(key, values):
+    init = time.time()
     start = 0
     end = len(values) - 1
     while(start <= end):
         half = start + (end - start)//2
         if(values[half] == key):
-            return True
+            return time.time() - init
         elif(values[half] < key):
             start = half + 1
         else:
             end = half - 1
-    return False
+    return -1
 
 # TODO BUSCA POR INTERPOLAÇÃO [O(log(log n))]
+# TODO BUSCA INDEXADA
 # https://marciobueno.com/arquivos/ensino/ed2/ED2_11_Pesquisa.pdf
 
 #INICIALIZA OS DICIONÁRIOS QUE CONTERÃO O Nº DE REGISTROS : TEMPO
-sequentialSearchTime = []
-binarySearchTime = []
-sequentialSearchSentinelTime = []
-registersQtt = []
+def main():
+    sequentialSearchTime = []
+    binarySearchTime = []
+    sequentialSearchSentinelTime = []
+    registersQtt = []
 
-for i in range(100):
-    keys = generateRandomUniqueNumbers(1, 1000, i)
+    keys = generateRandomUniqueNumbers(1, 9999999, MAX_REGISTRY)
 
-    registersQtt.append(i)
-    #no lugar de i+n deve ficar o tempo que demorou para cada método de busca encontrar a chave
-    sequentialSearchTime.append(i+50)
-    binarySearchTime.append(i+60)
-    sequentialSearchSentinelTime.append(i+70)
+    for i in range(10, MAX_REGISTRY, 100):
+        vector = keys[:i]
+        registersQtt.append(i)
 
-# PLOTANDO GRÁFICOS
-plt.plot(registersQtt, sequentialSearchTime)
-plt.plot(registersQtt, binarySearchTime)
-plt.plot(registersQtt, sequentialSearchSentinelTime)
-plt.ylabel('Tempo (ms)')
-plt.xlabel('Quantidade de Registros')
-plt.show()
+        auxVectorSequentialSearch = []
+        auxVectorBinarySearch = []
+        auxVectorSequentialSentinelSearch = []
+        
+        for n in range(10):
+            element = random.choice(vector)
 
+            auxVectorSequentialSearch.append(sequentialSearch(element, vector) * 1000000)
+            auxVectorSequentialSentinelSearch.append(sequentialSearchSentinel(element, vector.copy()) * 1000000)
+            auxVectorBinarySearch.append(binarySearch(element, vector) * 1000000)
 
+        # SAIU DO FOR PEGA OS VETORES AUXILIARES CALCULA A MEDIA E APPEND NOS VETORES DE TEMPO
+        # TODO no lugar de i+n deve ficar o tempo que demorou para cada método de busca encontrar a chave
+        sequentialSearchTime.append(averageVector(auxVectorSequentialSearch))
+        sequentialSearchSentinelTime.append(averageVector(auxVectorSequentialSentinelSearch))
+        binarySearchTime.append(averageVector(auxVectorBinarySearch))
+
+    # PLOTANDO GRÁFICOS
+    plt.plot(registersQtt, sequentialSearchTime)
+    plt.plot(registersQtt, binarySearchTime)
+    plt.plot(registersQtt, sequentialSearchSentinelTime)
+    plt.ylabel('Tempo (µs)')
+    plt.xlabel('Quantidade de Registros')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
