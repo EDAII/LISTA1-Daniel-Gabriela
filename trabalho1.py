820dc91548d5492cebb881475797572bdcd23ad3
DanMke <danmke@hotmail.com>
2019-08-27 19:09:41 -0300

    Added interpolation search and plot
    Signed-off-by: DanMke <danmke@hotmail.com>

--- trabalho1.py
+++ trabalho1.py
@@ -55,15 +55,40 @@ def binarySearch(key, values):
     return -1
 
 # TODO BUSCA POR INTERPOLAÇÃO [O(log(log n))]
+
+def interpolationSearch(key, values):
+    init = time.time()
+    lower = 0
+    upper = len(values) - 1
+
+    while lower <= upper and key >= values[lower] and key <= values[upper]:
+        if lower == upper:
+            if values[lower] == key:
+                return time.time() - init
+            return -1
+
+        position = lower + int((upper - lower) * ((key - values[lower]) / (values[upper] - values[lower])))
+
+        if values[position] == key:
+            return time.time() - init
+
+        if values[position] < key:
+            lower = position + 1
+        else:
+            upper = position - 1
+
+    return -1
+
 # TODO BUSCA INDEXADA
 # https://marciobueno.com/arquivos/ensino/ed2/ED2_11_Pesquisa.pdf
 
 #INICIALIZA OS DICIONÁRIOS QUE CONTERÃO O Nº DE REGISTROS : TEMPO
 def main():
-    sequentialSearchTime = []
-    binarySearchTime = []
-    sequentialSearchSentinelTime = []
     registersQtt = []
+    sequentialSearchTime = []
+    sequentialSearchSentinelTime = []
+    binarySearchTime = []
+    interpolationSearchTime = []
 
     keys = generateRandomUniqueNumbers(1, 9999999, MAX_REGISTRY)
 
@@ -74,6 +99,7 @@ def main():
         auxVectorSequentialSearch = []
         auxVectorBinarySearch = []
         auxVectorSequentialSentinelSearch = []
+        auxVectorInterpolationSearch = []
         
         for n in range(10):
             element = random.choice(vector)
@@ -81,18 +107,24 @@ def main():
             auxVectorSequentialSearch.append(sequentialSearch(element, vector) * 1000000)
             auxVectorSequentialSentinelSearch.append(sequentialSearchSentinel(element, vector.copy()) * 1000000)
             auxVectorBinarySearch.append(binarySearch(element, vector) * 1000000)
+            auxVectorInterpolationSearch.append(interpolationSearch(element, vector) * 1000000)
 
         # SAIU DO FOR PEGA OS VETORES AUXILIARES CALCULA A MEDIA E APPEND NOS VETORES DE TEMPO
         # TODO no lugar de i+n deve ficar o tempo que demorou para cada método de busca encontrar a chave
         sequentialSearchTime.append(averageVector(auxVectorSequentialSearch))
         sequentialSearchSentinelTime.append(averageVector(auxVectorSequentialSentinelSearch))
         binarySearchTime.append(averageVector(auxVectorBinarySearch))
+        interpolationSearchTime.append(averageVector(auxVectorInterpolationSearch))
 
     # PLOTANDO GRÁFICOS
-    plt.title("Search's")
+    plt.figure('Search methods')
+    plt.title("Search methods")
+
     plt.plot(registersQtt, sequentialSearchTime, label='Sequential Search')
     plt.plot(registersQtt, sequentialSearchSentinelTime, label='Sequential Sentinel Search')
     plt.plot(registersQtt, binarySearchTime, label='Binary Search')
+    plt.plot(registersQtt, interpolationSearchTime, label='Interpolation Search')
+
     plt.xlabel('Quantidade de Registros')
     plt.ylabel('Tempo (µs)')
     plt.legend()
