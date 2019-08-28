0ae269e42e8bf1ce9c17bb87a4ea52890421d149
DanMke <danmke@hotmail.com>
2019-08-27 22:15:32 -0300

    Added comments
    Signed-off-by: DanMke <danmke@hotmail.com>

--- trabalho1.py
+++ trabalho1.py
@@ -1,60 +1,76 @@
 import random
 import time
 import matplotlib.pyplot as plt
-from functools import reduce
 # INFO: 
 ## Todos os valores ordenados
 ## Sem repetição
 
 MAX_REGISTRY = 10000
 
-# TODO Colocar instalação do matplotlib no readme e requirements.txt
-
 def averageVector(values):
-    return reduce(lambda a, b: a + b, values) / len(values)
+    return sum(values) / len(values)
 
 # GERAR NÚMEROS ALEATÓRIOS EXCLUSIVOS DENTRO DE UM INTERVALO
 def generateRandomUniqueNumbers(start, end, quantity):
     numbers = random.sample(range(start, end), quantity)
     numbers.sort()
+
     return numbers
 
 # BUSCA SEQUENCIAL COM SENTINELA [O(n)] (Está inserindo a sentinela na lista passada por parametro)
 def sequentialSearchSentinel(key, values):
     init = time.time()
+
     i = 0
     values.append(key)
-    pos = len(values) - 1
+
     while (values[i] != key):
         i += 1
-    if i == (len(values) -1):
+
+    if i == (len(values) - 1):
         return -1
+
     return time.time() - init
 
 # BUSCA SEQUENCIAL SEM SENTINELA [O(n)]
 def sequentialSearch(key, values):
     init = time.time()
-    for i in values:
-        if (key == i):
+    i = 0
+    while i < len(values):
+        if (values[i] == key):
             return time.time() - init
+        i += 1
     return -1
+
+# Dessa forma ela roda mais rapido, sendo mais rapido que com sentinela
+# def sequentialSearch(key, values):
+#     init = time.time()
+#     for i in values:
+#         if (key == i):
+#             return time.time() - init
+#     return -1
 
 # BUSCA BINÁRIA (COM VETOR) [O(log n)]
 def binarySearch(key, values):
     init = time.time()
+
     start = 0
     end = len(values) - 1
+
     while(start <= end):
         half = start + (end - start)//2
+
         if(values[half] == key):
             return time.time() - init
+
         elif(values[half] < key):
             start = half + 1
+
         else:
             end = half - 1
     return -1
 
-# BUSCA POR INTERPOLAÇÃO [O(log(log n))]
+# BUSCA POR INTERPOLAÇÃO [O(log(log n))] (chaves uniformemente distribuídas)
 def interpolationSearch(key, values):
     init = time.time()
     lower = 0
@@ -118,11 +134,8 @@ def plotLineChart(registersQuantity, timeDict):
     plt.legend()
     plt.show()
 
-#INICIALIZA OS DICIONÁRIOS QUE CONTERÃO O Nº DE REGISTROS : TEMPO
 def main():
-
     timeResults, registersQtt = getTimeResults(generateRandomUniqueNumbers(0, 99999, MAX_REGISTRY))
-    
     plotLineChart(registersQtt, timeResults)
 
 if __name__ == '__main__':
